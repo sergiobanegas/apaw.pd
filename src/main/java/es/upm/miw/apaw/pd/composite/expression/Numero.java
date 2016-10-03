b8b0ac438d446d653a192949c90cadc31d38c49d
sergiobanegas <sbanegas92@gmail.com>
2016-10-03 13:14:38 +0200

    Create Numero:Numero

--- src/main/java/es/upm/miw/apaw/pd/composite/expression/Numero.java
+++ src/main/java/es/upm/miw/apaw/pd/composite/expression/Numero.java
@@ -4,6 +4,10 @@ public class Numero implements Expression {
 
     private int number;
 
+    public Numero(int number) {
+        this.number = number;
+    }
+
     @Override
     public int operar() {
         return number;
