7bba72898985950c5d5837f988ccd8f4e5ba3d28
sergiobanegas <sbanegas92@gmail.com>
2016-10-06 10:08:41 +0200

    Calculator: Initial commit

---
+++ src/main/java/es/upm/miw/apaw/pd/calculator/Calculator.java
@@ -0,0 +1,29 @@
+package es.upm.miw.apaw.pd.calculator;
+
+public class Calculator {
+    private int total;
+
+    public Calculator() {
+        this.reset();
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    protected void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void add(int valor) {
+        this.setTotal(this.total + valor);
+    }
+
+    public void subtract(int valor) {
+        this.setTotal(this.total - valor);
+    }
+
+    public void reset() {
+        this.setTotal(0);
+    }
+}
